29da2e9554c5f71929c2eaeebbcdd3665aa64071
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-22 18:17:27 +0530

    nextjs scrapped

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HeroSection from "@/app/components/HeroSection";
-
-export default function Home() {
-  return (
-    <div className="h-screen w-full">
-      <HeroSection />
-    </div>
-  );
-}
